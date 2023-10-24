044f940de3b1c18fe55a74e552768212820da5c9
Irene <irenemanningm@gmail.com>
2023-10-24 09:33:40 -0500

    done

--- src/features/bands/BandInput.js
+++ src/features/bands/BandInput.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function BandInput() {
-  return <div>Band Input</div>;
+function BandInput({onBandSubmit, name, setName}) {
+  function handleChange(e) {
+    setName(e.target.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={onBandSubmit} action="">
+        <label>name:</label>
+        <input type="text" onChange={handleChange} value={name} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default BandInput;
--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { bandAdded } from "./bandsSlice";
+import BandInput from "./BandInput";
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+  const [name, setName] = useState("")
+  const bandNames = useSelector((state)=>state.bands.entities)
+  const dispatch = useDispatch()
+  function onBandSubmit(e) {
+    e.preventDefault()
+    dispatch(bandAdded(name))
+    setName("")
+  }
+
+  return (
+    <div>BandsContainer 
+      <BandInput onBandSubmit={onBandSubmit} name={name} setName={setName}/> 
+      {bandNames.map((band, index)=> (
+        <li key={index} >{band}</li>
+      ))}
+    </div>
+  )
 }
 
 export default BandsContainer;
